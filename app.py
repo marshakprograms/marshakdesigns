52ba3ae9b4a021e657d3864d558e43b65e3f341d
Marsha Shearin <marshakprogrammer@gmail.com>
2026-05-25 16:35:29 -0400

    Add mail timeout handling for Railway deployment

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ if not app.config['SECRET_KEY']:
 app.config['MAIL_SERVER']   = 'smtp.gmail.com'
 app.config['MAIL_PORT']     = 587
 app.config['MAIL_USE_TLS']  = True
+app.config['MAIL_TIMEOUT'] = 10
+app.config['MAIL_SUPPRESS_SEND'] = False
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
